00f0dae8d17545f19c3cf60a6aa6ba75547dc1d8
Aarnav Srivastava <aarnav.srivastava18@gmail.com>
2022-10-22 19:50:52 -0400

    aditya

--- 64-bit-website/pages/members.js
+++ 64-bit-website/pages/members.js
@@ -3,6 +3,7 @@ import atharv from "../images/atharv.jpg"
 import james from "../images/james1.jpg";
 import aditya from "../images/aditya.jpg"
 import aarnav from "../images/aarnav.jpeg"
+import logo_transparent from "../logos/logo_transparent.png"
 
 export default function Members() {
     return (
@@ -21,12 +22,25 @@ export default function Members() {
               and has significant experience with both classical and jazz styles. He hopes his playing and arranging will bring a unique 
               flavor to the combo which will keep its performances interesting from start to finish.
             </Biography>
-            <Biography right={true} name="Aditya Balas" image={aditya}>ksdjfnkj sdkjn skdjfn sofij aoskjn sdkfns djfn ksjdfn </Biography>
+            <Biography right={true} name="Aditya Balas" image={aditya}>
+              Aditya Balas is the bassist of the 64 Bit Jazz Combo. He has had 9 years of experience across violin, 
+              piano, and bass guitar. Although he was initially trained on classical music, he has recently been 
+              expanding his talents to cover other genres such as rock, metal, and jazz.
+            </Biography>
             <Biography name="James Haber" image={james}>
               James Haber is the drummer, founder, and one of the principal arrangers of of The 64 Bit Jazz Combo.
               He has been playing drums for eight years and has enjoyed music his entire life.
               While originally trained as a rock drummer, he is comfortable playing a wide range of genres and enjoys learning new styles.
               He hopes his style of playing will contribute positively to the success of the group.
+            </Biography>
+            <Biography right={true} name="Anant Chary" image={logo_transparent}>
+              Coming soon...
+            </Biography>
+            <Biography name="Sayan Chakraborty" image={logo_transparent}>
+              Coming soon...
+            </Biography>
+            <Biography right={true} name="Chase Yetter" image={logo_transparent}>
+              Coming soon...
             </Biography>
           </div>
       </div>
