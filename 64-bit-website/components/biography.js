431dfe1c743aa551e0b2fcb490c18b1a4fb4365b
Aarnav Srivastava <aarnav.srivastava18@gmail.com>
2022-10-29 10:53:54 -0400

    img performance

--- 64-bit-website/components/biography.js
+++ 64-bit-website/components/biography.js
@@ -5,7 +5,7 @@ const Biography = (props) => {
     return (
         <div className="border-b-4 border-amber-400 flex relative flex-row justify-between w-full font-Inter text-black">
             {props.right && <div className="basis-5/12 border-r-4 border-amber-400 flex justify-center">
-                <Image priority="true" object-fit="true" src={props.image} className="absolute transition ease-in-out hover:scale-110 grow " id="gangsta" alt={props.name} />
+                <Image placeholder="blur" priority="true" object-fit="true" src={props.image} className="absolute transition ease-in-out hover:scale-110 grow " id="gangsta" alt={props.name} />
             </div>}
             <div className="basis-0 grow flex flex-col justify-center items-center gap-[2vw] text-center">
                 <div className="md:text-[2.5vw] text-md font-RobotoC">
@@ -16,7 +16,7 @@ const Biography = (props) => {
                 </div>
             </div>
             {!props.right && <div className="basis-5/12 border-l-4 border-amber-400 flex justify-center">
-                <Image priority="true" object-fit="true" src={props.image} className="absolute transition ease-in-out hover:scale-110 grow " id="gangsta" alt={props.name} />
+                <Image placeholder="blur" priority="true" object-fit="true" src={props.image} className="absolute transition ease-in-out hover:scale-110 grow " id="gangsta" alt={props.name} />
             </div>}
         </div>
     )
