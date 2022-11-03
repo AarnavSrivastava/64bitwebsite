544acb3dcb5f946a0ef14441ea071bf194035f16
Aarnav Srivastava <aarnav.srivastava18@gmail.com>
2022-11-03 18:52:46 -0400

    form

---
+++ 64-bit-website/components/form.js
@@ -0,0 +1,9 @@
+export default function Form() {
+    return (
+        <form className="flex flex-col justify-center items-center">
+            <div className="">
+
+            </div>
+        </form>
+    )
+}
